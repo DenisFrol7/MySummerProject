388face3deb0f9c7808abf26819489ffb45ecd28
Denis Frolov <domino387@gmail.com>
2022-07-04 22:35:34 +0300

    first

---
+++ src/main/java/com/example/mysummerproject/MySummerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysummerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySummerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySummerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mysummerproject/MySummerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysummerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySummerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
